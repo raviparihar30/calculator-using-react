61c5fe5c0c9d230f01dabdc78530fe846571dc01
raviparihar-rv <59733114+raviparihar-rv@users.noreply.github.com>
2022-11-18 19:41:15 +0530

    fix PR comments

--- src/Components/calculatorComponent.js
+++ src/Components/calculatorComponent.js
@@ -1,7 +1,9 @@
-import { Container } from "@mui/system";
+import { Typography } from "@mui/material";
 import { useState } from "react";
-import { Button, Col, Row } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import swal from "sweetalert";
+
+import styles from "./calculator.module.css";
 
 const Calculator = (props) => {
   const [input, setInput] = useState("0");
@@ -15,24 +17,21 @@ const Calculator = (props) => {
 
   const calculateAns = () => {
     try {
-      let ans = eval(input);
+      const ans = eval(input);
       setInput(ans);
     } catch (err) {
       swal("error", "Wrong Expression, try again", "error");
       setInput("0");
     }
   };
-  console.log(input);
 
   return (
     <div
-      className="flex alignItems-center"
-      style={{ border: 1, width: "300px", margin: "auto" }}
+      className={`flex alignItems-center ${styles.main}`}
+      // style={{ border: 1, width: "300px", margin: "auto" }}
     >
       <div className="text-center">
-        <h1>
-          Calculator
-        </h1>
+        <Typography variant="h2"> Calculator</Typography>
       </div>
       <Row>
         <Col className="fw-bold bg-secondary text-white border border-dark  p-4 text-end">
@@ -42,34 +41,26 @@ const Calculator = (props) => {
       <Row>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center cursor-pointer"
-          onClick={(e) => {
-            setInput("0");
-          }}
+          onClick={() => setInput("0")}
         >
           AC
         </Col>
 
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("+");
-          }}
+          onClick={() => handleclick("+")}
         >
           +
         </Col>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("%");
-          }}
+          onClick={() => handleclick("%")}
         >
           %
         </Col>
         <Col
           className="fw-bold bg-warning text-white border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("/");
-          }}
+          onClick={() => handleclick("/")}
         >
           /
         </Col>
@@ -78,33 +69,25 @@ const Calculator = (props) => {
       <Row>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("7");
-          }}
+          onClick={() => handleclick("7")}
         >
           7
         </Col>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("8");
-          }}
+          onClick={() => handleclick("8")}
         >
           8
         </Col>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("9");
-          }}
+          onClick={() => handleclick("9")}
         >
-          9{" "}
+          9
         </Col>
         <Col
           className="fw-bold bg-warning text-white border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("*");
-          }}
+          onClick={() => handleclick("*")}
         >
           X
         </Col>
@@ -112,33 +95,25 @@ const Calculator = (props) => {
       <Row>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("4");
-          }}
+          onClick={() => handleclick("4")}
         >
           4
         </Col>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("5");
-          }}
+          onClick={() => handleclick("5")}
         >
           5
         </Col>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("6");
-          }}
+          onClick={() => handleclick("6")}
         >
           6
         </Col>
         <Col
           className="fw-bold bg-warning text-white border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("-");
-          }}
+          onClick={() => handleclick("-")}
         >
           -
         </Col>
@@ -147,25 +122,19 @@ const Calculator = (props) => {
         <Col
           xs="6"
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick("0");
-          }}
+          onClick={() => handleclick("0")}
         >
           0
         </Col>
         <Col
           className="fw-bold bg-light border border-dark  p-4 text-center"
-          onClick={(e) => {
-            handleclick(".");
-          }}
+          onClick={() => handleclick(".")}
         >
           .
         </Col>
         <Col
           className="fw-bold bg-warning text-white border border-dark  p-4 text-center"
-          onClick={(e) => {
-            calculateAns();
-          }}
+          onClick={calculateAns}
         >
           =
         </Col>
